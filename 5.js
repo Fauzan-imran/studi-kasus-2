6b1e50da7306661f271b9e6d4ad47f4cd9df80df
MSI <fauzanimran314@gmail.com>
2026-08-03 14:38:00 +0800

    penyelesaian

--- 5.js
+++ 5.js
@@ -4,7 +4,7 @@ const buku = [20, 15, 35, 10, 5] ;
 function cekRestockGudang(id, buku) {
   for (let i = 0; i < id.length; i++) {
     if (buku[i] < 15) {
-      console.log(`Barang dengan ID ${id[i]} perlu di-restock`);
+      console.log(`Barang dengan ID ${id[i]} perlu di-restock karena stok kurang dari 15`);
     } else {
       console.log(`Barang dengan ID ${id[i]} masih cukup`);
     }
